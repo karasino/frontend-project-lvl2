795721db99ef0c375b06065ffc127244496434d9
Big Gun <karasino@mail.ru>
2020-12-07 16:25:45 +0300

    Add comparison logic

--- gendiff.js
+++ gendiff.js
@@ -1,11 +1,37 @@
 #!/usr/bin/env node
 import { Command } from "commander";
+import { readFileSync } from "fs";
+import { cwd } from "process";
+import { resolve } from "path";
+import _ from "lodash";
 
 const program = new Command();
 program
 	.version('1.0.0')
 	.arguments('<filepath1> <filepath2>')
 	.description('Compares two configuration files and shows a difference.')
-	.option('-f, --format [type]', 'output format');
+	.option('-f, --format [type]', 'output format')
+	.action((filepath1, filepath2) => {
+		const rawData1 = readFileSync(resolve(cwd(), filepath1));
+		const rawData2 = readFileSync(resolve(cwd(), filepath2));
+		const data1 = JSON.parse(rawData1);
+		const data2 = JSON.parse(rawData2);
+		const uniqueKeys = _.uniq([...Object.keys(data1), ...Object.keys(data2)]);
+		const result = uniqueKeys.reduce((acc, key) => {
+			if (data1.includes(key) && data2.includes(key)) {
+				if (data1[key] === data2[key]) {
+					acc.push(`${key}: ${data1[key]}`);
+				} else {
+					acc.push(`- ${key}: ${data1[key]}`);
+					acc.push(`+ ${key}: ${data2[key]}`);
+				}
+			} else if (data1.includes(key)) {
+				acc.push(`- ${key}: ${data1[key]}`);
+			} else if (data2.includes(key)) {
+				acc.push(`+ ${key}: ${data2[key]}`);
+			}
+			return result.join('/n');
+		});
+	});
 
 program.parse(process.argv);
