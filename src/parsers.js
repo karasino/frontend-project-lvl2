e96b71fa85620ea47e54a4f8c2a19dded98616e0
Big Gun <karasino@mail.ru>
2021-04-20 19:58:28 +0300

    Remove node modules from repository

--- src/parsers.js
+++ src/parsers.js
@@ -1,18 +1,14 @@
 import yaml from 'js-yaml';
 
-const getJsonData = (rawData) => JSON.parse(rawData);
-
-const getYamlData = (rawData) => yaml.safeLoad(rawData);
-
 const validExtensions = ['json', 'yml'];
+const parsers = {
+  json: JSON.parse,
+  yml: yaml.safeLoad,
+};
 
 export default (rawData, dataType) => {
   if (!validExtensions.includes(dataType)) {
     throw new Error('Unsupported file extension!');
   }
-  const parsers = {
-    json: getJsonData,
-    yml: getYamlData,
-  };
   return parsers[dataType](rawData);
 };
